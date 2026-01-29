9022897a3784bd89c6324f84d3799f6776e99178
Primica <arthur.malfere@gmail.com>
2026-01-29 15:28:06 +0100

    correction d'erreurs syntaxiques dnas le fichier vueMonProfil.php

--- vue/vueMonProfil.php
+++ vue/vueMonProfil.php
@@ -3,22 +3,24 @@
 
 Mon adresse électronique : <?= $util["mailU"] ?> <br />
 Mon pseudo : <?= $util["pseudoU"] ?> <br />
-Date inscription : <?= $util["dateInscU"];
+Date inscription : <?= $util["dateInscU"] ?>
 
 <hr>
 
 les restaurants que j'aime : <br />
-<?php for($i=0;$i<count($mesRestosAimes);$i++){ ?>
-    <a href="./?action=detail&idR=<?= $mesRestosAimes[$i]["idR"] ?>"><?= $mesRestosAimes[$i]["nomR"] ?></a><br />
+<?php for ($i = 0; $i < count($mesRestosAimes); $i++) { ?>
+    <a href="./?action=detail&idR=<?= $mesRestosAimes[$i][
+        "idR"
+    ] ?>"><?= $mesRestosAimes[$i]["nomR"] ?></a><br />
 <?php } ?>
 <hr>
-les types de cuisine que j'aime : 
-<ul id="tagFood">		
-<?php for($i=0;$i<count($mesTypeCuisineAimes);$i++){ ?>
-    <li class="tag"><span class="tag">#</span><?= $mesTypeCuisineAimes[$i]["libelleTC"] ?></li>
+les types de cuisine que j'aime :
+<ul id="tagFood">
+<?php for ($i = 0; $i < count($mesTypeCuisineAimes); $i++) { ?>
+    <li class="tag"><span class="tag">#</span><?= $mesTypeCuisineAimes[$i][
+        "libelleTC"
+    ] ?></li>
 <?php } ?>
 </ul>
 <hr>
 <a href="./?action=deconnexion">se deconnecter</a>
-
-
